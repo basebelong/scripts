89e7e45645b91bcedb5c36143261e55d68099e86
basebelong <jonas@hendrikson.se>
2021-09-07 10:57:42 +0200

    Battery charging status

---
+++ postat/postat.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+#use strict;
+#use warnings;
+
+my $status;
+open(my $fh, '<', '/sys/class/power_supply/BAT1/status');
+{
+    local $/;
+    $status = <$fh>;
+}
+
+my $en_now;
+open(my $fh, '<', '/sys/class/power_supply/BAT1/energy_now');
+{
+    local $/;
+    $en_now = <$fh>;
+}
+close($fh);
+
+my $pow_now;
+open(my $fh, '<', '/sys/class/power_supply/BAT1/power_now');
+{
+    local $/;
+    $pow_now = <$fh>;
+}
+close($fh);
+
+my $percent;
+open(my $fh, '<', '/sys/class/power_supply/BAT1/capacity');
+{
+    local $/;
+    $percent = <$fh>;
+}
+close($fh);
+
+my $h_left = $en_now / $pow_now;
+
+my @split_h=split('\.', $h_left);
+
+my $hours=@split_h[0];
+my $minutes=@split_h[1]*60;
+$minutes=substr($minutes, 0, 2);
+chomp $percent;
+
+print("Battery: " . $percent . "\%\n" . $status . "Time: " . $hours . ':' . $minutes . "\n");
+
